08ae94bc489827761f1c4b164a3e1ba7c04a4a60
Athena Yao <athena@fastmail.fm>
2015-06-16 19:40:06 -0700

    Add secret/signature_valid check to webhook

--- ghi_assist/handlers.py
+++ ghi_assist/handlers.py
@@ -1,9 +1,28 @@
 """
 Webhook for Github events.
 """
+import hmac
+from hashlib import sha1
+
 class Webhook(object):
-    def __init__(self):
+    def __init__(self, secret=None):
         self.handlers = {}
+        self.secret = secret
+
+    def signature_valid(self, data=None, signed_data=None, digest=sha1):
+        """
+        Checks if the signature from github matches the expected.
+
+        Args:
+            data: string of request data
+            signed_data: signature provided by github
+            digest: digest module to use (only sha1 is supported currently)
+        Returns:
+            True if our calculated signature matches the signature provided by Github.
+            False otherwise.
+        """
+        mac = hmac.new(self.secret, msg=data, digestmod=digest)
+        return hmac.compare_digest(mac.hexdigest(), signed_data)
 
     def register(self, event, handler):
         """
